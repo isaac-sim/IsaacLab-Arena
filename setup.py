f073726a54a229f23748e7cfc12d769cdcc8fd61
Clemens Volk <cvolk@nvidia.com>
2025-07-16 18:55:11 +0200

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -8,55 +8,15 @@
 # without an express license agreement from NVIDIA CORPORATION or
 # its affiliates is strictly prohibited.
 #
-"""Installation script for the 'omni.isaac.lab_tasks' python package."""
+"""Installation script for the 'isaac_arena' python package."""
 
-import itertools
-import os
+from setuptools import find_packages, setup
 
-from setuptools import setup
-import toml  # type: ignore[import-untyped]
-
-# Obtain the extension data from the extension.toml file
-EXTENSION_PATH = os.path.dirname(os.path.realpath(__file__))
-# Read the extension.toml file
-EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "extension.toml"))
-
-# Minimum dependencies required prior to installation
-INSTALL_REQUIRES = [
-    # generic
-    "numpy",
-    "h5py",
-]
-
-# PYTORCH_INDEX_URL = ['https://downkload.pytorch.org/whl/cu118']
-
-# Extra dependencies for RL agents
-EXTRAS_REQUIRE: dict[str, list[str]] = {}
-
-# Cumulation of all extra-requires
-all_requirements = list(itertools.chain.from_iterable(EXTRAS_REQUIRE.values()))
-# Remove duplicates in the all list to avoid double installations
-EXTRAS_REQUIRE["all"] = list(set(all_requirements))
-
-# Installation operation
 setup(
     name="isaac_arena",
-    author="Isaac Lab Project Developers",
-    maintainer="Isaac Lab Project Developers",
-    url=EXTENSION_TOML_DATA["package"]["repository"],
-    version=EXTENSION_TOML_DATA["package"]["version"],
-    description=EXTENSION_TOML_DATA["package"]["description"],
-    keywords=EXTENSION_TOML_DATA["package"]["keywords"],
-    include_package_data=True,
+    version="0.1.0",
+    description="Isaac Arena environment for Robotics Simulations. ",
+    packages=find_packages(),
     python_requires=">=3.10",
-    install_requires=INSTALL_REQUIRES,
-    extras_require=EXTRAS_REQUIRE,
-    packages=["isaac_arena"],
-    classifiers=[
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.10",
-        "Isaac Sim :: 4.2.0",
-        "Isaac Sim :: 4.1.0",
-    ],
     zip_safe=False,
 )
