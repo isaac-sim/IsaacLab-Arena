5fdda281c697347af54bfeb12b232ff00ddda3fb
Xinjie Yao <xyao@nvidia.com>
2025-09-26 15:45:03 -0700

    wip

---
+++ isaac_arena/policy/gr00t/policy_config.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from dataclasses import dataclass, field
+from pathlib import Path
+
+
+@dataclass
+class LerobotReplayActionPolicyConfig:
+    # model specific parameters
+    language_instruction: str = field(
+        default="", metadata={"description": "Instruction given to the policy in natural language."}
+    )
+    dataset_path: str = field(default="", metadata={"description": "Full path to the dataset directory."})
+    action_horizon: int = field(
+        default=16, metadata={"description": "Number of actions in the policy's predictionhorizon."}
+    )
+    embodiment_tag: str = field(
+        default="new_embodiment",
+        metadata={
+            "description": (
+                "Identifier for the robot embodiment used in the policy inference (e.g., 'gr1' or 'new_embodiment')."
+            )
+        },
+    )
+    data_config: str = field(
+        default="unitree_g1_sim_wbc", metadata={"description": "Name of the data configuration to use for the policy."}
+    )
+    gr00t_joints_config_path: Path = field(
+        default=Path(__file__).parent.resolve() / "g1" / "gr00t_43dof_joint_space.yaml",
+        metadata={"description": "Path to the YAML file specifying the joint ordering configuration for GR00T policy."},
+    )
+
+    # robot simulation specific parameters
+    action_joints_config_path: Path = field(
+        default=Path(__file__).parent.resolve() / "g1" / "43dof_joint_space.yaml",
+        metadata={
+            "description": (
+                "Path to the YAML file specifying the joint ordering configuration for GR1 action space in Lab."
+            )
+        },
+    )
+    state_joints_config_path: Path = field(
+        default=Path(__file__).parent.resolve() / "g1" / "43dof_joint_space.yaml",
+        metadata={
+            "description": (
+                "Path to the YAML file specifying the joint ordering configuration for GR1 state space in Lab."
+            )
+        },
+    )
+
+    # Default to GPU policy and CPU physics simulation
+    policy_device: str = field(
+        default="cuda", metadata={"description": "Device to run the policy model on (e.g., 'cuda' or 'cpu')."}
+    )
+
+
+    # Closed loop specific parameters
+    num_feedback_actions: int = field(
+        default=16,
+        metadata={
+            "description": "Number of feedback actions to execute per rollout (can be less than action_horizon)."
+        },
+    )
+
+    def __post_init__(self):
+        assert (
+            self.num_feedback_actions <= self.action_horizon
+        ), "num_feedback_actions must be less than or equal to action_horizon"
+        # assert all paths exist
+        assert Path(self.gr00t_joints_config_path).exists(), "gr00t_joints_config_path does not exist"
+        assert Path(self.action_joints_config_path).exists(), "action_joints_config_path does not exist"
+        assert Path(self.state_joints_config_path).exists(), "state_joints_config_path does not exist"
+        assert Path(self.dataset_path).exists(), "dataset_path does not exist. Do not use relative paths."
+        # embodiment_tag
+        assert self.embodiment_tag in [
+            "gr1",
+            "new_embodiment",
+        ], "embodiment_tag must be one of the following: " + ", ".join(["gr1", "new_embodiment"])
--- isaac_arena/policy/gr00t/replay_lerobot_action_policy.py
+++ isaac_arena/policy/gr00t/replay_lerobot_action_policy.py
@@ -11,17 +11,51 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import gymnasium as gym
 import torch
 from abc import ABC, abstractmethod
 from gymnasium.spaces.dict import Dict as GymSpacesDict
 
+from gr00t.data.dataset import LeRobotSingleDataset
 
 class ReplayLerobotActionPolicy(ABC):
-    def __init__(self):
+    def __init__(self, policy_config: dict):
         """
         Base class for replay action policies from Lerobot dataset.
         """
+        self.policy_config = policy_config
+        self.policy = self._load_policy()
+        self.policy_iter = iter(self.policy)
+        self._load_policy_joints_config()
+        self._load_sim_joints_config()
+
+    def _load_policy_joints_config(self):
+        """Load the policy joint config from the data config."""
+        self.gr00t_joints_config = load_robot_joints_config(self.policy_config.gr00t_joints_config_path)
+
+    def _load_sim_joints_config(self):
+        """Load the simulation joint config from the data config."""
+        self.g1_state_joints_config = load_robot_joints_config(self.policy_config.state_joints_config_path)
+        self.g1_action_joints_config = load_robot_joints_config(self.policy_config.action_joints_config_path)
+
+    def _load_policy(self):
+        """Load the policy from the model path."""
+        assert os.path.exists(self.args.dataset_path), f"Dataset path {self.args.dataset_path} does not exist"
+
+        # Use the same data preprocessor as the loaded fine-tuned ckpts
+        self.data_config = DATA_CONFIG_MAP[self.args.data_config]
+
+        modality_config = self.data_config.modality_config()
+
+        return LeRobotSingleDataset(
+            dataset_path=self.args.dataset_path,
+            modality_configs=modality_config,
+            video_backend=self.args.video_backend,
+            video_backend_kwargs=None,
+            transforms=None,  # We'll handle transforms separately through the policy
+            embodiment_tag=self.args.embodiment_tag,
+        )
 
     @abstractmethod
     def get_action(self, env: gym.Env, observation: GymSpacesDict) -> torch.Tensor:
