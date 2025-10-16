dc7ba03ebb6b29164878c05f119502415a7ab381
Jay Yang <jay.yang@lightwheel.ai>
2025-10-17 06:13:10 +0800

    implement get object reference from USD

--- isaac_arena/assets/object_reference.py
+++ isaac_arena/assets/object_reference.py
@@ -13,27 +13,33 @@
 # limitations under the License.
 
 
+from pxr import Usd, UsdPhysics, UsdGeom, UsdSkel
 from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg
 from isaaclab.sensors.contact_sensor.contact_sensor_cfg import ContactSensorCfg
 
 from isaac_arena.affordances.openable import Openable
 from isaac_arena.assets.asset import Asset
 from isaac_arena.assets.object_base import ObjectBase, ObjectType
+from isaac_arena.utils.pose import Pose
 
 
 class ObjectReference(ObjectBase):
     """An object which *refers* to an existing element in the scene"""
 
     def __init__(self, parent_asset: Asset, **kwargs):
-        # TODO(alexmillane, 2025.09.08): Need some way of extracting the initial pose from the USD file.
         super().__init__(**kwargs)
         if parent_asset:
             self._check_path_in_parent_usd(parent_asset)
         self.parent_asset = parent_asset
+        parent_stage = Usd.Stage.Open(parent_asset.usd_path)
+        reference_prim = self._get_prim_by_name(parent_stage.GetPseudoRoot(), self.name)[0]
+        if not reference_prim:
+            raise ValueError(f"Reference prim {self.name} not found in parent USD file {parent_asset.usd_path}")
+        reference_pos, reference_quat = self._get_prim_pos_rot_in_world(reference_prim)
+        self.initial_pose = Pose(position_xyz=tuple(reference_pos), rotation_wxyz=tuple(reference_quat))
 
-    def get_initial_pose(self) -> None:
-        # TODO(alexmillane, 2025.09.08): Need some way of extracting the initial pose from the USD file.
-        return None
+    def get_initial_pose(self) -> Pose:
+        return self.initial_pose
 
     def get_contact_sensor_cfg(self, contact_against_prim_paths: list[str] | None = None) -> ContactSensorCfg:
         # NOTE(alexmillane): Right now this requires that the object
@@ -48,6 +54,10 @@ class ObjectReference(ObjectBase):
         assert self.object_type == ObjectType.RIGID
         object_cfg = RigidObjectCfg(
             prim_path=self.prim_path,
+            init_state=RigidObjectCfg.InitialStateCfg(
+                pos=self.initial_pose.position_xyz,
+                rot=self.initial_pose.rotation_wxyz,
+            ),
         )
         return object_cfg
 
@@ -56,6 +66,10 @@ class ObjectReference(ObjectBase):
         object_cfg = ArticulationCfg(
             prim_path=self.prim_path,
             actuators={},
+            init_state=ArticulationCfg.InitialStateCfg(
+                pos=self.initial_pose.position_xyz,
+                rot=self.initial_pose.rotation_wxyz,
+            ),
         )
         return object_cfg
 
@@ -63,12 +77,41 @@ class ObjectReference(ObjectBase):
         assert self.object_type == ObjectType.BASE
         object_cfg = AssetBaseCfg(
             prim_path=self.prim_path,
+            init_state=AssetBaseCfg.InitialStateCfg(
+                pos=self.initial_pose.position_xyz,
+                rot=self.initial_pose.rotation_wxyz,
+            ),
         )
         return object_cfg
 
     def _check_path_in_parent_usd(self, parent_asset: Asset) -> bool:
         # TODO(alexmillane, 2025.09.08): Implement this check!
         return True
+
+    def _get_prim_pos_rot_in_world(self, prim):
+        """Get prim position, rotation and scale in world coordinates"""
+        xformable = UsdGeom.Xformable(prim)
+        if not xformable:
+            return None, None
+        matrix = xformable.ComputeLocalToWorldTransform(Usd.TimeCode.Default())
+        try:
+            pos, rot, _ = UsdSkel.DecomposeTransform(matrix)
+            pos_list = list(pos)
+            quat_list = [rot.GetReal(), rot.GetImaginary()[0], rot.GetImaginary()[1], rot.GetImaginary()[2]]  # wxyz
+            return pos_list, quat_list
+        except Exception as e:
+            print(f"Error decomposing transform for {prim.GetName()}: {e}")
+            return None, None
+
+    def _get_prim_by_name(self, prim, name, only_xform=True):
+        """Get prim by name"""
+        result = []
+        if prim.GetName().lower() == name.lower():
+            if not only_xform or prim.GetTypeName() == "Xform":
+                result.append(prim)
+        for child in prim.GetAllChildren():
+            result.extend(self._get_prim_by_name(child, name, only_xform))
+        return result
 
 
 class OpenableObjectReference(ObjectReference, Openable):
