30e37cfa2f1b3c9c24ce6c660bf688f762debb00
Vikram Ramasamy <158473438+viiik-inside@users.noreply.github.com>
2025-11-03 15:23:14 +0100

    Remove wrongfully declared abstract classes (#185)
    ## Summary
    remove some abstract classes which were defined unintentionally

--- isaaclab_arena/embodiments/embodiment_base.py
+++ isaaclab_arena/embodiments/embodiment_base.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from abc import abstractmethod
 from typing import Any
 
 from isaaclab.envs import ManagerBasedRLMimicEnv
@@ -46,7 +45,6 @@ class EmbodimentBase(Asset):
     def set_initial_pose(self, pose: Pose) -> None:
         self.initial_pose = pose
 
-    @abstractmethod
     def get_scene_cfg(self) -> Any:
         if self.initial_pose is not None:
             self.scene_config = self._update_scene_cfg_with_robot_initial_pose(self.scene_config, self.initial_pose)
@@ -59,11 +57,9 @@ class EmbodimentBase(Asset):
                 )
         return self.scene_config
 
-    @abstractmethod
     def get_action_cfg(self) -> Any:
         return self.action_config
 
-    @abstractmethod
     def get_observation_cfg(self) -> Any:
         if self.enable_cameras:
             if self.camera_config is not None:
@@ -75,25 +71,17 @@ class EmbodimentBase(Asset):
                 )
         return self.observation_config
 
-    @abstractmethod
     def get_events_cfg(self) -> Any:
         return self.event_config
 
-    @abstractmethod
     def get_mimic_env(self) -> ManagerBasedRLMimicEnv:
         return self.mimic_env
 
-    @abstractmethod
     def get_xr_cfg(self) -> Any:
         return self.xr
 
-    @abstractmethod
     def get_camera_cfg(self) -> Any:
         return self.camera_config
-
-    @abstractmethod
-    def get_retargeters_cfg(self, retargeter_name: str) -> Any:
-        raise NotImplementedError("Function not implemented yet.")
 
     def _update_scene_cfg_with_robot_initial_pose(self, scene_config: Any, pose: Pose) -> Any:
         if scene_config is None or not hasattr(scene_config, "robot"):
@@ -105,7 +93,6 @@ class EmbodimentBase(Asset):
     def get_recorder_term_cfg(self) -> RecorderManagerBaseCfg:
         return None
 
-    @abstractmethod
     def get_termination_cfg(self) -> Any:
         return self.termination_cfg
 
