326ebdb6779d8bf45a3f242589d83b32902365ef
viiik-inside <vramasamy@nvidia.com>
2025-11-27 16:53:52 +0100

    fix flake errors in test

--- isaaclab_arena/tests/test_object_configuration.py
+++ isaaclab_arena/tests/test_object_configuration.py
@@ -3,15 +3,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-
 from isaaclab_arena.tests.utils.subprocess import run_simulation_app_function
 
 HEADLESS = True
 
+
 def _test_object_initial_pose_update(simulation_app):
 
-    from isaaclab_arena.assets.object import Object
     from isaaclab_arena.assets.asset_registry import AssetRegistry
     from isaaclab_arena.utils.pose import Pose
 
@@ -20,7 +18,6 @@ def _test_object_initial_pose_update(simulation_app):
     rigid_object = asset_registry.get_asset_by_name("cracker_box")()
     # Disable debug visualization, this is True by default.
     rigid_object.object_cfg.debug_vis = False
-    initial_pose = rigid_object.get_initial_pose()
 
     # Now lets add an initial pose to the object.
     new_initial_pose = Pose(position_xyz=(5.0, 0.0, 0.0), rotation_wxyz=(1.0, 0.0, 0.0, 0.0))
@@ -28,9 +25,10 @@ def _test_object_initial_pose_update(simulation_app):
 
     # Now lets check that the initial pose has been updated and that the debug visualization is still disabled.
     assert rigid_object.get_initial_pose() == new_initial_pose
-    assert rigid_object.object_cfg.debug_vis == False
+    assert rigid_object.object_cfg.debug_vis is False
 
     return True
+
 
 def test_object_configuration():
     result = run_simulation_app_function(
@@ -38,6 +36,7 @@ def test_object_configuration():
         headless=HEADLESS,
     )
     assert result, "Test failed"
+
 
 if __name__ == "__main__":
     test_object_configuration()
