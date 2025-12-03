52862ef9b31f4745223bc2ebf89932b9d147332a
alex <amillane@nvidia.com>
2025-12-03 10:31:51 +0100

    Reenable everything.

--- isaaclab_arena/tests/policy/test_replay_lerobot_action_policy.py
+++ isaaclab_arena/tests/policy/test_replay_lerobot_action_policy.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import pytest
+
 from isaaclab_arena.tests.utils.constants import TestConstants
 from isaaclab_arena.tests.utils.subprocess import run_subprocess
 
@@ -13,34 +15,36 @@ NUM_STEPS = 10
 TRAJECTORY_INDEX = 0
 
 
-# def test_g1_locomanip_replay_lerobot_policy_runner_single_env():
-#     args = [TestConstants.python_path, f"{TestConstants.examples_dir}/policy_runner.py"]
-#     args.append("--policy_type")
-#     args.append("replay_lerobot")
-#     args.append("--config_yaml_path")
-#     args.append(TestConstants.test_data_dir + "/test_g1_locomanip_lerobot/test_g1_locomanip_replay_action_config.yaml")
-#     args.append("--max_steps")
-#     args.append(str(NUM_STEPS))
-#     args.append("--trajectory_index")
-#     args.append(str(TRAJECTORY_INDEX))
-#     if HEADLESS:
-#         args.append("--headless")
-#     if ENABLE_CAMERAS:
-#         args.append("--enable_cameras")
-#     # example env
-#     args.append("galileo_g1_locomanip_pick_and_place")
-#     args.append("--object")
-#     args.append("brown_box")
-#     args.append("--embodiment")
-#     args.append("g1_wbc_joint")
-#     run_subprocess(args)
+def test_g1_locomanip_replay_lerobot_policy_runner_single_env():
+    args = [TestConstants.python_path, f"{TestConstants.examples_dir}/policy_runner.py"]
+    args.append("--policy_type")
+    args.append("replay_lerobot")
+    args.append("--config_yaml_path")
+    args.append(TestConstants.test_data_dir + "/test_g1_locomanip_lerobot/test_g1_locomanip_replay_action_config.yaml")
+    args.append("--max_steps")
+    args.append(str(NUM_STEPS))
+    args.append("--trajectory_index")
+    args.append(str(TRAJECTORY_INDEX))
+    if HEADLESS:
+        args.append("--headless")
+    if ENABLE_CAMERAS:
+        args.append("--enable_cameras")
+    # example env
+    args.append("galileo_g1_locomanip_pick_and_place")
+    args.append("--object")
+    args.append("brown_box")
+    args.append("--embodiment")
+    args.append("g1_wbc_joint")
+    run_subprocess(args)
+
+
+pytest.mark.skip(reason="Fails on CI for reasons under investigation.")
 
 
 def test_gr1_manip_replay_lerobot_policy_runner_single_env():
     args = [TestConstants.python_path, f"{TestConstants.examples_dir}/policy_runner.py"]
     args.append("--policy_type")
     args.append("replay_lerobot")
-    # args.append("zero_action") # TESTED: SWITCHED FOR ZERO_ACTION
     args.append("--config_yaml_path")
     args.append(TestConstants.test_data_dir + "/test_gr1_manip_lerobot/test_gr1_manip_replay_action_config.yaml")
     args.append("--max_steps")
@@ -54,12 +58,12 @@ def test_gr1_manip_replay_lerobot_policy_runner_single_env():
     # example env
     args.append("gr1_open_microwave")
     args.append("--object")
-    args.append("microwave")  # TESTED: REMOVED
+    args.append("microwave")
     args.append("--embodiment")
-    args.append("gr1_joint")  # TESTED: SWITCHED FOR GR1_JOINT
+    args.append("gr1_joint")
     run_subprocess(args)
 
 
 if __name__ == "__main__":
-    # test_g1_locomanip_replay_lerobot_policy_runner_single_env()
+    test_g1_locomanip_replay_lerobot_policy_runner_single_env()
     test_gr1_manip_replay_lerobot_policy_runner_single_env()
