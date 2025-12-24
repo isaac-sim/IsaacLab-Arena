kanghui0204/IsaacLab-Arena-vln

Add factory_assembly_task and the relevant task examples

isaaclab_arena_environments/mdp/env_callbacks.py
@@ -1,0 +1,67 @@
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Environment configuration callbacks for manipulation tasks."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from isaaclab_arena.environments.isaaclab_arena_manager_based_env import IsaacLabArenaManagerBasedRLEnvCfg
+
+
+def factory_assembly_env_cfg_callback(env_cfg: IsaacLabArenaManagerBasedRLEnvCfg) -> IsaacLabArenaManagerBasedRLEnvCfg:
+    """
+    Environment configuration callback optimized for factory assembly tasks.
+    
+    This callback modifies the simulation settings to provide better stability
+    and precision for tasks like peg insertion, gear meshing, and other fine
+    manipulation operations.
+    
+    Args:
+        env_cfg: The environment configuration to modify.
+        
+    Returns:
+        The modified environment configuration.
+    """
+    from isaaclab.sim import SimulationCfg, PhysxCfg
+    from isaaclab.sim.spawners.materials import RigidBodyMaterialCfg
+    
+    # Simulation settings optimized for factory assembly tasks
+    env_cfg.sim = SimulationCfg(
+        dt=1 / 60,  # 60Hz - balance between speed and stability
+        render_interval=2,
+        physx=PhysxCfg(
+            solver_type=1,
+            max_position_iteration_count=192,  # Important to avoid interpenetration
+            max_velocity_iteration_count=1,
+            bounce_threshold_velocity=0.2,
+            friction_offset_threshold=0.01,
+            friction_correlation_distance=0.00625,
+            gpu_max_rigid_contact_count=2**23,
+            gpu_max_rigid_patch_count=2**23,
+            gpu_max_num_partitions=1,  # Important for stable simulation
+        ),
+        physics_material=RigidBodyMaterialCfg(
+            static_friction=1.0,
+            dynamic_friction=1.0,
+        ),
+    )
+    
+    # Control frequency = 60Hz / 2 = 30Hz
+    env_cfg.decimation = 2
+    
+    return env_cfg
+

isaac-sim/IsaacLab-Arena

isaaclab_arena_environments/mdp/env_callbacks.py
@@ -1,0 +1,67 @@
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Environment configuration callbacks for manipulation tasks."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from isaaclab_arena.environments.isaaclab_arena_manager_based_env import IsaacLabArenaManagerBasedRLEnvCfg
+
+
+def assembly_env_cfg_callback(env_cfg: IsaacLabArenaManagerBasedRLEnvCfg) -> IsaacLabArenaManagerBasedRLEnvCfg:
+    """
+    Environment configuration callback optimized for factory assembly tasks.
+    
+    This callback modifies the simulation settings to provide better stability
+    and precision for tasks like peg insertion, gear meshing, and other fine
+    manipulation operations.
+    
+    Args:
+        env_cfg: The environment configuration to modify.
+        
+    Returns:
+        The modified environment configuration.
+    """
+    from isaaclab.sim import SimulationCfg, PhysxCfg
+    from isaaclab.sim.spawners.materials import RigidBodyMaterialCfg
+    
+    # Simulation settings optimized for factory assembly tasks
+    env_cfg.sim = SimulationCfg(
+        dt=1 / 60,  # 60Hz - balance between speed and stability
+        render_interval=2,
+        physx=PhysxCfg(
+            solver_type=1,
+            max_position_iteration_count=192,  # Important to avoid interpenetration
+            max_velocity_iteration_count=1,
+            bounce_threshold_velocity=0.2,
+            friction_offset_threshold=0.01,
+            friction_correlation_distance=0.00625,
+            gpu_max_rigid_contact_count=2**23,
+            gpu_max_rigid_patch_count=2**23,
+            gpu_max_num_partitions=1,  # Important for stable simulation
+        ),
+        physics_material=RigidBodyMaterialCfg(
+            static_friction=1.0,
+            dynamic_friction=1.0,
+        ),
+    )
+    
+    # Control frequency = 60Hz / 2 = 30Hz
+    env_cfg.decimation = 2
+    
+    return env_cfg
+