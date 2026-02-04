ddd9813dd9fd9d4e1dad4e0088228d836c6eecb2
Peter Du <peterd@nvidia.com>
2026-02-04 10:10:02 -0800

    revert compile env notebook.py changes

--- isaaclab_arena/examples/compile_env_notebook.py
+++ isaaclab_arena/examples/compile_env_notebook.py
@@ -15,136 +15,29 @@ print("Launching simulation app once in notebook")
 simulation_app = AppLauncher()
 
 from isaaclab_arena.assets.asset_registry import AssetRegistry
-from isaaclab_arena.assets.object_reference import ObjectReference, OpenableObjectReference
 from isaaclab_arena.cli.isaaclab_arena_cli import get_isaaclab_arena_cli_parser
 from isaaclab_arena.environments.arena_env_builder import ArenaEnvBuilder
 from isaaclab_arena.environments.isaaclab_arena_environment import IsaacLabArenaEnvironment
+from isaaclab_arena.relations.relations import IsAnchor, On
 from isaaclab_arena.scene.scene import Scene
-from isaaclab_arena.tasks.close_door_task import CloseDoorTask
-from isaaclab_arena.tasks.pick_and_place_task import PickAndPlaceTask
-from isaaclab_arena.tasks.sequential_task_base import SequentialTaskBase
-from isaaclab_arena.tasks.task_base import TaskBase
-from isaaclab_arena.utils.pose import Pose, PoseRange
+from isaaclab_arena.utils.pose import Pose
 
 asset_registry = AssetRegistry()
 
-embodiment = asset_registry.get_asset_by_name("gr1_pink")()
+background = asset_registry.get_asset_by_name("kitchen")()
+embodiment = asset_registry.get_asset_by_name("franka")()
 cracker_box = asset_registry.get_asset_by_name("cracker_box")()
 tomato_soup_can = asset_registry.get_asset_by_name("tomato_soup_can")()
 
-# pickup_object_name = "sweet_potato"
-pickup_object_name = "jug"
-pickup_object = asset_registry.get_asset_by_name(pickup_object_name)()
+cracker_box.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
+cracker_box.add_relation(IsAnchor())
+tomato_soup_can.add_relation(On(cracker_box))
 
-# from isaaclab_arena.relations.relations import IsAnchor, On
-# cracker_box.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
-# cracker_box.add_relation(IsAnchor())
-# tomato_soup_can.add_relation(On(cracker_box))
-
-kitchen_background = asset_registry.get_asset_by_name("lightwheel_robocasa_kitchen")(style_id=2)
-
-light = asset_registry.get_asset_by_name("light")()
-
-# Refrigerator
-refrigerator = OpenableObjectReference(
-    name="refrigerator",
-    prim_path="{ENV_REGEX_NS}/lightwheel_robocasa_kitchen/fridge_main_group",
-    parent_asset=kitchen_background,
-    openable_joint_name="fridge_door_joint",
-    openable_threshold=0.5,
-)
-
-# Refrigerator (shelf)
-refrigerator_shelf = ObjectReference(
-    name="refrigerator_shelf",
-    prim_path="{ENV_REGEX_NS}/lightwheel_robocasa_kitchen/fridge_main_group/Refrigerator034",
-    parent_asset=kitchen_background,
-)
-
-embodiment.set_initial_pose(
-    Pose(
-        # position_xyz=(3.943, -1.069, 0.995),
-        position_xyz=(3.943, -1.0, 0.995),
-        # rotation_wxyz=(0.7071068, 0.0, 0.0, 0.7071068)
-        # [ 0, 0, 0.5, 0.8660254 ]
-        rotation_wxyz=(0.8660254, 0.0, 0.0, 0.5),
-    )
-)
-
-
-RANDOMIZATION_HALF_RANGE_X_M = 0.04
-RANDOMIZATION_HALF_RANGE_Y_M = 0.01
-RANDOMIZATION_HALF_RANGE_Z_M = 0.0
-z_position = {
-    "sweet_potato": 1.0,
-    "jug": 1.1,
-}[pickup_object_name]
-yaw = {
-    "sweet_potato": 0.0,
-    "jug": -90.0,
-}[pickup_object_name]
-pickup_object.set_initial_pose(
-    # Bench (no randomization)
-    # Pose(position_xyz=(3.922, -0.565, 1.019), rotation_wxyz=(0.7071068, 0.0, 0.0, 0.7071068))
-    # Bench (with randomization)
-    PoseRange(
-        position_xyz_min=(
-            4.1 - RANDOMIZATION_HALF_RANGE_X_M,
-            -0.6 - RANDOMIZATION_HALF_RANGE_Y_M,
-            z_position - RANDOMIZATION_HALF_RANGE_Z_M,
-        ),
-        position_xyz_max=(
-            4.1 + RANDOMIZATION_HALF_RANGE_X_M,
-            -0.6 + RANDOMIZATION_HALF_RANGE_Y_M,
-            z_position + RANDOMIZATION_HALF_RANGE_Z_M,
-        ),
-        # position_xyz_max=(
-        #     3.922 + RANDOMIZATION_HALF_RANGE_X_M,
-        #     -0.565 + RANDOMIZATION_HALF_RANGE_Y_M,
-        #     1.019 + RANDOMIZATION_HALF_RANGE_Z_M
-        # ),
-        rpy_min=(0.0, 0.0, yaw),
-        rpy_max=(0.0, 0.0, yaw),
-    )
-    # Above shelf
-    # Pose(
-    #     position_xyz=(4.625, -0.395, 1.224),
-    #     rotation_wxyz=(0.7071068, 0.0, 0.0, 0.7071068)
-    # )
-)
-
-
-class PutAndCloseDoorTask(SequentialTaskBase):
-
-    def __init__(
-        self,
-        subtasks: list[TaskBase],
-        episode_length_s: float | None = None,
-    ):
-        super().__init__(subtasks=subtasks, episode_length_s=episode_length_s)
-
-    def get_viewer_cfg(self):
-        return self.subtasks[0].get_viewer_cfg()
-
-    def get_prompt(self):
-        return None
-
-    def get_mimic_env_cfg(self, arm_mode):
-        return None
-
-
-pick_and_place_task = PickAndPlaceTask(pickup_object, refrigerator_shelf, kitchen_background)
-close_door_task = CloseDoorTask(refrigerator, closedness_threshold=0.05, reset_openness=0.5)
-
-task = PutAndCloseDoorTask(subtasks=[pick_and_place_task, close_door_task])
-
-scene = Scene(assets=[kitchen_background, refrigerator, refrigerator_shelf, pickup_object, light])
+scene = Scene(assets=[background, cracker_box, tomato_soup_can])
 isaaclab_arena_environment = IsaacLabArenaEnvironment(
     name="reference_object_test",
     embodiment=embodiment,
     scene=scene,
-    task=task,
-    teleop_device=None,
 )
 
 args_cli = get_isaaclab_arena_cli_parser().parse_args([])
@@ -153,90 +46,13 @@ env_builder = ArenaEnvBuilder(isaaclab_arena_environment, args_cli)
 env = env_builder.make_registered()
 env.reset()
 
-
-STEPS_UNTIL_TELEPORT_VEGETABLE = 50
-STEPS_UNTIL_CLOSE_DOOR = 100
-
-# %%
-
-# Reset frequently to check the randomizations.
-
-# Run some zero actions.
-STEPS_BETWEEN_RESETS = 30
-NUM_STEPS = 200
-for idx in tqdm.tqdm(range(NUM_STEPS)):
-    with torch.inference_mode():
-        actions = torch.zeros(env.action_space.shape, device=env.unwrapped.device)
-        _, _, terminated, _, _ = env.step(actions)
-        if idx % STEPS_BETWEEN_RESETS == 0:
-            env.reset()
-
-
 # %%
 
 # Run some zero actions.
-NUM_STEPS = 200
+NUM_STEPS = 1000
 for _ in tqdm.tqdm(range(NUM_STEPS)):
     with torch.inference_mode():
         actions = torch.zeros(env.action_space.shape, device=env.unwrapped.device)
-
-        if _ == STEPS_UNTIL_TELEPORT_VEGETABLE:
-            # Teleport the vegetable to the shelf
-            pickup_object.set_object_pose(
-                env,
-                env_ids=None,
-                pose=Pose(position_xyz=(4.625, -0.395, 1.224), rotation_wxyz=(0.7071068, 0.0, 0.0, 0.7071068)),
-            )
-
-        if _ == STEPS_UNTIL_CLOSE_DOOR:
-            # Close the refrigerator, completing the task
-            refrigerator.close(env, env_ids=None, percentage=0.0)
-
-        _, _, terminated, _, _ = env.step(actions)
-
-        if terminated:
-            print("SUCCESS!!!!!!")
-
-# %%
-
-# Debugging the robot joint names
-num_joints = len(env.scene["robot"].joint_names)
-print(f"Number of joints: {num_joints}")
-print(env.scene["robot"].joint_names)
-
-# %%
-
-# Inspect the vegetables in the registry
-
-from lightwheel_sdk.loader import object_loader
-
-# Print all the vegetables in the registry
-for asset in object_loader.list_registry():
-    print(asset["name"])
-    # properties = asset["property"]
-    # if "types" in properties:
-    #     types = properties["types"]
-    #     if "vegetable" in types:
-    #         print(asset)
-
-# %%
-
-object_names = []
-for asset in object_loader.list_registry():
-    object_names.append(asset["name"])
-print(object_names)
-
-
-# %%
-
-from lightwheel_sdk.loader import object_loader
-
-file_path, object_name, metadata = object_loader.acquire_by_registry(
-    registry_type="objects", registry_name=["jug"], file_type="USD"
-)
-print(f"loaded {object_name} at {file_path}")
-
-# %%
-
+        env.step(actions)
 
 # %%
