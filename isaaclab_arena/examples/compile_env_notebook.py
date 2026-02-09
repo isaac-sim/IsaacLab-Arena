9120e794d5d13a8e3b2d192bd65444e482861191
alex <amillane@nvidia.com>
2026-02-09 15:54:47 +0100

    Revert the compile env notebook.

--- isaaclab_arena/examples/compile_env_notebook.py
+++ isaaclab_arena/examples/compile_env_notebook.py
@@ -11,23 +11,15 @@ import tqdm
 import pinocchio  # noqa: F401
 from isaaclab.app import AppLauncher
 
-from isaaclab_arena.cli.isaaclab_arena_cli import get_isaaclab_arena_cli_parser
-
 print("Launching simulation app once in notebook")
-# args = get_isaaclab_arena_cli_parser().parse_args(["--num_envs", "2"])
-# simulation_app = AppLauncher(args)
 simulation_app = AppLauncher()
 
 from isaaclab_arena.assets.asset_registry import AssetRegistry
-from isaaclab_arena.assets.object_base import ObjectType
-from isaaclab_arena.assets.object_reference import ObjectReference
-from isaaclab_arena.assets.object_set import RigidObjectSet
 from isaaclab_arena.cli.isaaclab_arena_cli import get_isaaclab_arena_cli_parser
 from isaaclab_arena.environments.arena_env_builder import ArenaEnvBuilder
 from isaaclab_arena.environments.isaaclab_arena_environment import IsaacLabArenaEnvironment
 from isaaclab_arena.relations.relations import IsAnchor, On
 from isaaclab_arena.scene.scene import Scene
-from isaaclab_arena.tasks.pick_and_place_task import PickAndPlaceTask
 from isaaclab_arena.utils.pose import Pose
 
 asset_registry = AssetRegistry()
@@ -36,38 +28,20 @@ background = asset_registry.get_asset_by_name("kitchen")()
 embodiment = asset_registry.get_asset_by_name("franka")()
 cracker_box = asset_registry.get_asset_by_name("cracker_box")()
 tomato_soup_can = asset_registry.get_asset_by_name("tomato_soup_can")()
-sweet_potato = asset_registry.get_asset_by_name("sweet_potato")()
-jug = asset_registry.get_asset_by_name("jug")()
 
-# cracker_box.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
-# cracker_box.add_relation(IsAnchor())
-# tomato_soup_can.add_relation(On(cracker_box))
+cracker_box.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
+cracker_box.add_relation(IsAnchor())
+tomato_soup_can.add_relation(On(cracker_box))
 
-object_set = RigidObjectSet(
-    name="object_set",
-    # objects=[cracker_box, tomato_soup_can]
-    objects=[sweet_potato, jug],
-)
-object_set.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
-destination_location = ObjectReference(
-    name="destination_location",
-    prim_path="{ENV_REGEX_NS}/kitchen/Cabinet_B_02",
-    parent_asset=background,
-    object_type=ObjectType.RIGID,
-)
-scene = Scene(assets=[background, object_set, destination_location])
+scene = Scene(assets=[background, cracker_box, tomato_soup_can])
 isaaclab_arena_environment = IsaacLabArenaEnvironment(
     name="reference_object_test",
     embodiment=embodiment,
     scene=scene,
-    task=PickAndPlaceTask(
-        pick_up_object=object_set, destination_location=destination_location, background_scene=background
-    ),
 )
 
 args_cli = get_isaaclab_arena_cli_parser().parse_args([])
 args_cli.solve_relations = True
-args_cli.num_envs = 2
 env_builder = ArenaEnvBuilder(isaaclab_arena_environment, args_cli)
 env = env_builder.make_registered()
 env.reset()
@@ -75,11 +49,10 @@ env.reset()
 # %%
 
 # Run some zero actions.
-NUM_STEPS = 500
+NUM_STEPS = 1000
 for _ in tqdm.tqdm(range(NUM_STEPS)):
     with torch.inference_mode():
         actions = torch.zeros(env.action_space.shape, device=env.unwrapped.device)
         env.step(actions)
-
 
 # %%
