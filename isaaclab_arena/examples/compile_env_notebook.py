81f8041d6ec8e1fcb6bc1069835b4d9f8391563b
alex <amillane@nvidia.com>
2026-02-17 11:27:44 +0100

    Revert the notebook.

--- isaaclab_arena/examples/compile_env_notebook.py
+++ isaaclab_arena/examples/compile_env_notebook.py
@@ -15,7 +15,6 @@ print("Launching simulation app once in notebook")
 simulation_app = AppLauncher()
 
 from isaaclab_arena.assets.asset_registry import AssetRegistry
-from isaaclab_arena.assets.object_set import RigidObjectSet
 from isaaclab_arena.cli.isaaclab_arena_cli import get_isaaclab_arena_cli_parser
 from isaaclab_arena.environments.arena_env_builder import ArenaEnvBuilder
 from isaaclab_arena.environments.isaaclab_arena_environment import IsaacLabArenaEnvironment
@@ -30,22 +29,11 @@ embodiment = asset_registry.get_asset_by_name("franka")()
 cracker_box = asset_registry.get_asset_by_name("cracker_box")()
 tomato_soup_can = asset_registry.get_asset_by_name("tomato_soup_can")()
 
-# cracker_box.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
-# cracker_box.add_relation(IsAnchor())
-# tomato_soup_can.add_relation(On(cracker_box))
+cracker_box.set_initial_pose(Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)))
+cracker_box.add_relation(IsAnchor())
+tomato_soup_can.add_relation(On(cracker_box))
 
-object_set = RigidObjectSet(
-    name="object_set",
-    objects=[cracker_box, tomato_soup_can],
-)
-# Different pose per object (object name -> Pose)
-object_set.set_initial_pose({
-    cracker_box.name: Pose(position_xyz=(0.4, 0.0, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)),
-    tomato_soup_can.name: Pose(position_xyz=(0.35, 0.50, 0.1), rotation_wxyz=(1.0, 0.0, 0.0, 0.0)),
-})
-
-# scene = Scene(assets=[background, cracker_box, tomato_soup_can])
-scene = Scene(assets=[background, object_set])
+scene = Scene(assets=[background, cracker_box, tomato_soup_can])
 isaaclab_arena_environment = IsaacLabArenaEnvironment(
     name="reference_object_test",
     embodiment=embodiment,
@@ -53,27 +41,18 @@ isaaclab_arena_environment = IsaacLabArenaEnvironment(
 )
 
 args_cli = get_isaaclab_arena_cli_parser().parse_args([])
-# args_cli.solve_relations = True
-args_cli.num_envs = 2
+args_cli.solve_relations = True
 env_builder = ArenaEnvBuilder(isaaclab_arena_environment, args_cli)
-name, cfg = env_builder.build_registered()
-
-env = env_builder.make_registered(cfg)
+env = env_builder.make_registered()
 env.reset()
 
 # %%
+
 # Run some zero actions.
-RESET_EVERY_N_STEPS = 100
-NUM_STEPS = 500
-for idx in tqdm.tqdm(range(NUM_STEPS)):
+NUM_STEPS = 1000
+for _ in tqdm.tqdm(range(NUM_STEPS)):
     with torch.inference_mode():
         actions = torch.zeros(env.action_space.shape, device=env.unwrapped.device)
         env.step(actions)
-        if idx % RESET_EVERY_N_STEPS == 0:
-            env.reset()
-
-
-# %%
-
 
 # %%
