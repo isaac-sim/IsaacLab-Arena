4feb5de7842e49640261dac925090fddaab3f132
Xinjie Yao <xyao@nvidia.com>
2026-04-01 16:26:17 -0700

    lint

--- isaaclab_arena/tests/test_scene_add_assets_ordering.py
+++ isaaclab_arena/tests/test_scene_add_assets_ordering.py
@@ -11,8 +11,6 @@ HEADLESS = True
 def _test_object_references_added_before_parents(simulation_app) -> bool:
     """ObjectReferences can be added before their parents."""
     from isaaclab_arena.assets.asset_registry import AssetRegistry
-    from isaaclab_arena.scene.scene import Scene
-
     from isaaclab_arena.assets.object_reference import ObjectReference
     from isaaclab_arena.scene.scene import Scene
 
@@ -20,12 +18,12 @@ def _test_object_references_added_before_parents(simulation_app) -> bool:
     asset_registry = AssetRegistry()
     table_background = asset_registry.get_asset_by_name("office_table")()
     table_reference = ObjectReference(
-       name="table",
-       prim_path="{ENV_REGEX_NS}/office_table/Geometry/sm_tabletop_a01_01/sm_tabletop_a01_top_01",
-       parent_asset=table_background,
-   )
+        name="table",
+        prim_path="{ENV_REGEX_NS}/office_table/Geometry/sm_tabletop_a01_01/sm_tabletop_a01_top_01",
+        parent_asset=table_background,
+    )
     try:
-        scene = Scene(assets=[table_reference, table_background])
+        Scene(assets=[table_reference, table_background])
     except AssertionError:
         return False
 
@@ -38,13 +36,14 @@ def _test_object_reference_without_parent_raises(simulation_app) -> bool:
     from isaaclab_arena.assets.asset_registry import AssetRegistry
     from isaaclab_arena.assets.object_reference import ObjectReference
     from isaaclab_arena.scene.scene import Scene
+
     asset_registry = AssetRegistry()
     table_background = asset_registry.get_asset_by_name("office_table")()
     table_reference = ObjectReference(
-       name="table",
-       prim_path="{ENV_REGEX_NS}/office_table/Geometry/sm_tabletop_a01_01/sm_tabletop_a01_top_01",
-       parent_asset=table_background,
-   )
+        name="table",
+        prim_path="{ENV_REGEX_NS}/office_table/Geometry/sm_tabletop_a01_01/sm_tabletop_a01_top_01",
+        parent_asset=table_background,
+    )
 
     try:
         Scene(assets=[table_reference])
@@ -52,6 +51,7 @@ def _test_object_reference_without_parent_raises(simulation_app) -> bool:
         return True
 
     return False
+
 
 def test_object_references_added_before_parents():
     result = run_simulation_app_function(
