c6e7677a49408193a7931f142b7b4e5f7f395f67
Xinjie Yao <xyao@nvidia.com>
2026-09-10 22:40:37 -0700

    round 4

--- isaaclab_arena/assets/object.py
+++ isaaclab_arena/assets/object.py
@@ -111,10 +111,10 @@ class Object(RootedObjectBase):
         # at the root of the USD file. To be robust to this, we find the shallowest rigid body and add the
         # contact sensor to it.
         contact_sensor_prim_path = self.get_contact_sensor_prim_path()
-        # There are also cases where the contact against object does not have its rigid body at the root.
-        # In that case, we also need to find the shallowest rigid body.
         if isinstance(contact_against_object, Object):
             # Handles Object and its subclasses, including RigidObjectSet.
+            # RigidObjectSet normalizes the USD paths for all members before spawning, so they have the same
+            # relative structure and rigid-body name. We add the contact sensor to the normalized rigid body beneath the its scene prim.
             filter_prim_paths = [contact_against_object.get_contact_sensor_prim_path()]
         elif isinstance(contact_against_object, ObjectBase):
             # Handles ObjectReference.
