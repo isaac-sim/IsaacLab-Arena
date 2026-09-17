60d00a4c780a2480487dada130b67940c31adc40
Qian Lin <qianl@nvidia.com>
2026-09-17 12:13:01 +0800

    import fix for app crash

--- isaaclab_arena/environment_spec/arena_env_graph_conversion_utils.py
+++ isaaclab_arena/environment_spec/arena_env_graph_conversion_utils.py
@@ -20,9 +20,9 @@ from isaaclab_arena.assets.object_type import ObjectType
 from isaaclab_arena.assets.registries import AssetRegistry, ObjectRelationLibraryRegistry
 from isaaclab_arena.environment_spec.arena_env_graph_task_conversion_utils import build_task_from_spec
 from isaaclab_arena.environment_spec.arena_env_graph_types import ObjectReferenceSpec, SpatialRelationSpec
-from isaaclab_arena.physics.physics_backend import PhysicsBackend
 from isaaclab_arena.relations.object_placer_params import ObjectPlacerParams
 from isaaclab_arena.relations.relation_solver_params import RelationSolverParams
+from isaaclab_arena.utils.physics_backend import PhysicsBackend
 from isaaclab_arena.utils.pose import Pose
 from isaaclab_arena.utils.usd_helpers import has_light, open_stage
 
