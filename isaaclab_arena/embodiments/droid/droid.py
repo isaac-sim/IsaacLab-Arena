3bba7e7a6f5c66ce1f2eb296e6ea5f1f13ac62c9
Xinjie Yao <xyao@nvidia.com>
2026-09-17 21:01:01 -0700

    Remove redundant DROID spawn comment
    Signed-off-by: Xinjie Yao <xyao@nvidia.com>

--- isaaclab_arena/embodiments/droid/droid.py
+++ isaaclab_arena/embodiments/droid/droid.py
@@ -624,7 +624,6 @@ def spawn_newton_droid(
 
     from isaaclab_arena.utils.usd.helpers import move_collision_schemas_to_meshes
 
-    # The outer @clone applies visibility, labels, and contact sensors after robot setup.
     prim = spawn_from_usd.__wrapped__(
         prim_path,
         spawner_cfg,
